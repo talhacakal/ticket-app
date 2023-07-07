5bf06bb82c45e8494c01f59991dbb10e43e40a21
Talha Çakal <talha.cakal1@gmail.com>
2023-07-07 15:02:22 +0300

    Eureka and gateway added

--- Gateway/src/main/java/com/gateway/GatewayApplication.java
+++ Gateway/src/main/java/com/gateway/GatewayApplication.java
@@ -1,4 +1,4 @@
-package com.ticketapp;
+package com.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,8 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class NotificationApplication {
+public class GatewayApplication {
     public static void main(String[] args) {
-        SpringApplication.run(NotificationApplication.class, args);
+        SpringApplication.run(GatewayApplication.class, args);
     }
 }
